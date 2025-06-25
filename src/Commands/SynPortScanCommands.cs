8941b46e8751f345083a74ee67b163d64487e9a8
joaostack <83355344+joaostack@users.noreply.github.com>
2025-06-25 18:47:12 -0300

    Some fixes

--- src/Commands/SynPortScanCommands.cs
+++ src/Commands/SynPortScanCommands.cs
@@ -1,1 +1,49 @@
+using SynPortScan.Core;
+
 namespace SynPortScan.Commands;
+
+public class SynPortScanCommands
+{
+    private readonly string _ip;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SynPortScanCommands"/> class.
+    /// </summary>
+    /// <param name="ip">The target IP address to scan.</param>
+    public SynPortScanCommands(string ip)
+    {
+        _ip = ip;
+    }
+
+    /// <summary>
+    /// Scans a specific port on a target host using SYN scan.
+    /// </summary>
+    /// <param name="targetHost">The target host IP address.</param>
+    /// <param name="port">The port number to scan.</param>
+    public void Execute()
+    {
+        try
+        {
+            var device = DeviceHelper.SelectDevice();
+            DeviceHelper.OpenDevice(device);
+            var mac = PacketBuilder.GetMacFromIP(device, _ip);
+
+            // add : on the mac address
+            var macString = string.Join(":", mac.GetAddressBytes().Select(b => b.ToString("X2")));
+            Console.WriteLine($"MAC address for {_ip}: {macString}");
+
+            PacketBuilder.SendSynPacket(device, _ip, 80);
+
+            device.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            Console.ResetColor();
+        }
+    }
+}
