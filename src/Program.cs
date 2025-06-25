d3f196ad931e9dfc11e57314fb09229ccc2b32f6
joaostack <83355344+joaostack@users.noreply.github.com>
2025-06-25 18:47:11 -0300

    Fix dir

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace SynPortScan;
+
+using System;
+using SynPortScan.Core;
+
+/// <summary>
+/// SynPortScan application.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// SynPortScan args.
+    /// </summary>
+    /// <param name="ip">Target IP</param>
+    static void Main(string ip)
+    {
+        Console.WriteLine("SynPortScan is a SYN port scanner.");
+
+        PacketBuilder.GetMacFromIP(null, ip);
+    }
+}
